3f36d569890b94290772c42d44727bcd7a6c6325
Francisco Gregorio de las Heras <francisco.gregoriodelasheras@gmail.com>
2021-05-21 22:15:14 +0200

    feat: create CitySearch component

---
+++ src/CitySearch.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+
+class CitySearch extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      query: '',
+      suggestions: [],
+    };
+  }
+
+  handleInputChanged = (event) => {
+    const value = event.target.value;
+    const suggestions = this.props.locations.filter((location) => {
+      return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+    });
+    this.setState({
+      query: value,
+      suggestions,
+    });
+  };
+
+  handleItemClicked = (suggestion) => {
+    this.setState({
+      query: suggestion,
+    });
+  };
+
+  render() {
+    return (
+      <div className='CitySearch'>
+        <input
+          type='text'
+          className='city'
+          value={this.state.query}
+          onChange={this.handleInputChanged}
+        />
+        <ul className='suggestions'>
+          {this.state.suggestions.map((suggestion) => (
+            <li
+              key={suggestion}
+              onClick={() => this.handleItemClicked(suggestion)}>
+              {suggestion}
+            </li>
+          ))}
+          <li key='all'>
+            <b>See all cities</b>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default CitySearch;
