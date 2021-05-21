f01102190530d653e0c82cd36307973b21e28ffe
Francisco Gregorio de las Heras <francisco.gregoriodelasheras@gmail.com>
2021-05-21 22:12:26 +0200

    feat: take events from Google Calendar API

---
+++ src/api.js
@@ -0,0 +1,6 @@
+// Take events from Google Calendar API, remove duplicates and create a new array
+export const extractLocations = (events) => {
+  var extractLocations = events.map((event) => event.location);
+  var locations = [...new Set(extractLocations)];
+  return locations;
+};
