d6c691c49708f517c31141d1bb82eeba5e93d020
Francisco Gregorio de las Heras <francisco.gregoriodelasheras@gmail.com>
2021-05-21 22:18:04 +0200

    test: create unit test script for EventList component

---
+++ src/__tests__/EventList.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import EventList from '../EventList';
+import Event from '../Event';
+import { mockData } from '../mock-data';
+
+describe('<EventList /> component', () => {
+  test('render correct number of events', () => {
+    const EventListWrapper = shallow(<EventList events={mockData} />);
+    expect(EventListWrapper.find(Event)).toHaveLength(mockData.length);
+  });
+});
