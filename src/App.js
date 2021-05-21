491486c02c5984f0805b20e74a9e2167cd6a5aae
Francisco Gregorio de las Heras <francisco.gregoriodelasheras@gmail.com>
2021-05-21 22:14:42 +0200

    feat: create App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 // import logo from '../public/img/Meetbot.svg';
 import './App.css';
+import EventList from './EventList';
+import CitySearch from './CitySearch';
 
-function App() {
-  return <div className='App'></div>;
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <CitySearch />
+        <EventList />
+      </div>
+    );
+  }
 }
 
 export default App;
