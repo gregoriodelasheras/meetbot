720a5f5ad87e2bbe79b879a2e3abcd651b4d606f
Francisco Gregorio de las Heras <francisco.gregoriodelasheras@gmail.com>
2021-05-21 22:17:25 +0200

    test: create unit test script for App component

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '../App';
+import EventList from '../EventList';
+import CitySearch from '../CitySearch';
+
+describe('<App /> component', () => {
+  let AppWrapper;
+  beforeAll(() => {
+    AppWrapper = shallow(<App />);
+  });
+
+  test('render list of events', () => {
+    expect(AppWrapper.find(EventList)).toHaveLength(1);
+  });
+
+  test('render CitySearch', () => {
+    expect(AppWrapper.find(CitySearch)).toHaveLength(1);
+  });
+});
