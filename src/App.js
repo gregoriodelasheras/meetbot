afe193d901a52b162deeacdd2153b724273f4d0f
Francisco Gregorio de las Heras <francisco.gregoriodelasheras@gmail.com>
2021-05-23 22:45:40 +0200

    feat: render NumberOfEvents in App

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import React, { Component } from 'react';
 import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
+import NumberOfEvents from './NumberOfEvents';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
+        <h1>Meetbot</h1>
         <CitySearch />
+        <NumberOfEvents />
         <EventList />
       </div>
     );
