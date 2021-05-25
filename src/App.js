a3a18bcf66dc375922315d51b07aaf5c25c1161a
Francisco Gregorio de las Heras <francisco.gregoriodelasheras@gmail.com>
2021-05-25 18:24:47 +0200

    style: add new logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import logo from '../public/img/Meetbot.svg';
+import logo from './logo.svg';
 import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
@@ -44,6 +44,7 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
+        <img src={logo} alt='Logo' className='img-fluid logo-web' />
         <h1>Meetbot</h1>
         <CitySearch
           locations={this.state.locations}
