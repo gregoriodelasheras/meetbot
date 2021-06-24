3749b8eba9d3fb4d54d8e1e65a86eca01daccd28
Francisco Gregorio de las Heras <francisco.gregoriodelasheras@gmail.com>
2021-06-24 14:21:29 +0200

    refactor: export checkToken function

--- src/api.js
+++ src/api.js
@@ -5,7 +5,7 @@ import NProgress from 'nprogress';
 // 1 - Authentication and authorization
 
 // Check the token's validity.
-const checkToken = async (accessToken) => {
+export const checkToken = async (accessToken) => {
   const result = await fetch(
     `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`,
   )
