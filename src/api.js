954084c9f182e8950ddd285e1fa5f31529e68570
Francisco Gregorio de las Heras <francisco.gregoriodelasheras@gmail.com>
2021-06-25 02:11:00 +0200

    fix: solve problem loading the app offline (catch error)

--- src/api.js
+++ src/api.js
@@ -10,7 +10,7 @@ export const checkToken = async (accessToken) => {
     `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`,
   )
     .then((res) => res.json())
-    .catch((error) => error.json());
+    .catch((error) => error);
 
   return result;
 };
